fc9bdb1c42bbdef73899225a81b573a7cc3692ca
aidaiomurbek <ermekov-rustam@mail.ru>
2021-02-18 17:12:56 +0500

    23

--- app/src/main/java/com/example/firsthmand2/ui/profileFragment.java
+++ app/src/main/java/com/example/firsthmand2/ui/profileFragment.java
@@ -16,7 +16,7 @@ import android.widget.ImageView;
 import com.example.firsthmand2.R;
 
 public class profileFragment extends Fragment {
-    private Boolean check;
+    private Boolean check2;
     private ImageView imageView;
 
     private ActivityResultLauncher mGetContent;
