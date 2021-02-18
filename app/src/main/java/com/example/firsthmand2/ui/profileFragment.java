784516a41723563fa0d18505b6125f4751486094
aidaiomurbek <ermekov-rustam@mail.ru>
2021-02-18 17:10:16 +0500

    first one

---
+++ app/src/main/java/com/example/firsthmand2/ui/profileFragment.java
@@ -0,0 +1,45 @@
+package com.example.firsthmand2.ui;
+
+import android.os.Bundle;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.firsthmand2.R;
+
+public class profileFragment extends Fragment {
+    private ImageView imageView;
+
+    private ActivityResultLauncher mGetContent;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view,
+                              @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        imageView = view.findViewById(R.id.imageView);
+        mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+                uri-> imageView.setImageURI(uri));
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGetContent.launch("image/*");     //launch-означает зайди в галерею и *-вытащи любую фотку
+            }
+        });
+    }
+}
